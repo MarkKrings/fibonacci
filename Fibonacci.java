fb6f30e8163a6edd67c8cb1941a06b8db1820fd8
Mark <95907950+MarkKrings@users.noreply.github.com>
2022-07-05 17:07:11 +0200

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,19 @@
+public class Fibonacci {
+
+    public static long fibonacci(int a) {
+        if (a==1 || a==2) {
+            return 1;
+        } else return fibonacci(a-1) + fibonacci(a-2);
+    }
+
+    public static long fibo(long z) {
+
+        if (z == 0) {
+            return 0;
+        } else if (z == 1) {
+            return 1;
+        } else {
+            return fibo(z - 1) + fibo(z - 2);
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Fibonacci: "+Fibonacci.fibonacci(15));
+
+        System.out.println("----------------\nFibonacci 2: "+Fibonacci.fibo(14));
+    }
+}
